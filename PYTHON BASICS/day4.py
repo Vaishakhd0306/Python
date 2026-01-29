f08b098491732802c1b95aabf9f8a6252dd4d87c
Vaishak H D <vaishakhd0306@gmail.com>
2026-01-29 18:00:11 +0530

    day4

---
+++ PYTHON BASICS/day4.py
@@ -0,0 +1,16 @@
+# arr = [1,2,3,4,5]
+
+# print(arr)
+# print(len(arr))
+# arr.append(6)
+# print(arr)
+# arr.pop()
+# print(arr)
+
+# print(sum(arr))
+# print(min(arr))
+# print(max(arr))
+
+
+arrInp = input()
+print(arrInp)
