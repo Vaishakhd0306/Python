54b1796cd862a730fde1723f60a8661ac079071f
Vaishak H D <vaishakhd0306@gmail.com>
2026-02-02 09:50:56 +0530

    adding slicing

---
+++ PYTHON BASICS/day5.py
@@ -0,0 +1,9 @@
+inputText = input("Enter the string")
+
+print("string :", inputText)
+print("s[0]",inputText[0] if inputText else "No text")
+
+# Python Slicing:s[start:stop:step]
+
+
+print("reverseString:", inputText[::-1])
